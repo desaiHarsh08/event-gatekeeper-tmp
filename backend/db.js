895bc3220a10f4786a23315c38f3f043c4c262f6
harshNileshDesai <harsh.desai@kasisto.com>
2023-12-26 12:11:38 +0530

    third commit

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,8 @@ import dotenv from 'dotenv';
 
 dotenv.config({path: './config.env'});
 
-const mongoURI = process.env.MONGO_URI_LOCAL;
+// const mongoURI = process.env.MONGO_URI_LOCAL;
+const mongoURI = process.env.MONGO_URI;
 
 const connectToMongo = async () => {
     try {
